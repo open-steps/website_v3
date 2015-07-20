5349cf2081b8c32f00b8511c71f09e7ce755b487
Open-Steps.org <contact@open-steps.org>
2015-07-20 11:33:06 +0200

    implementing sitemap

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,13 @@
-(function($){
-  $('#timeline .item').on('click',function(){
-   $(this).class('.item');
+(function($) {
+ $('#timeline .item').on('click', function() {
+  $(this).class('.item');
+ });
+
+ $('.sitemap').on('click', function() {
+  console.log("sitemap");
+  $("#sitemap").slideDown("fast", function() {
+   // Animation complete.
   });
+ });
+
 })(jQuery); // end of jQuery name space
