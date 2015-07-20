28894f11aee473f0002c3858e49f9aa28883a77c
Alex Corbi <a.corbi@gmail.com>
2015-07-20 13:05:14 +0200

    implementing sitemap

--- js/logic.js
+++ js/logic.js
@@ -1,13 +1,29 @@
 (function($) {
- $('#timeline .item').on('click', function() {
-  $(this).class('.item');
- });
 
- $('.sitemap').on('click', function() {
-  console.log("sitemap");
-  $("#sitemap").slideDown("fast", function() {
-   // Animation complete.
+  var sitemap_open = false;
+
+  $('#timeline .item').on('click', function() {
+    $(this).class('.item');
   });
- });
+
+  $('#sitemap-open').on('click', function() {
+    $("#sitemap").slideDown("slow", function() {
+      console.log("sitemap opened");
+      $("#past").hide();
+      $("#present").hide();
+      $("#friends").hide();
+      $("#beyond").hide();
+    });
+  });
+
+  $('#sitemap-close').on('click', function() {
+    $("#past").show();
+    $("#present").show();
+    $("#friends").show();
+    $("#beyond").show();
+    $("#sitemap").slideUp("slow", function() {
+      console.log("sitemap closed");
+    });
+  });
 
 })(jQuery); // end of jQuery name space
