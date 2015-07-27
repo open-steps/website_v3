77179c752a5d8533885b76dde2e107eaf8cc722d
Open-Steps.org <contact@open-steps.org>
2015-07-27 09:40:50 +0200

    added friends icons. improved sitemap

--- js/logic.js
+++ js/logic.js
@@ -1,29 +1,46 @@
 (function($) {
 
-  var sitemap_open = false;
+ var sitemap_open = false;
+ var sitemap_hidden = false;
 
-  $('#timeline .item').on('click', function() {
-    $(this).class('.item');
-  });
+ $('#timeline .item').on('click', function() {
+  $(this).class('.item');
+ });
 
-  $('#sitemap-open').on('click', function() {
-    $("#sitemap").slideDown("slow", function() {
-      console.log("sitemap opened");
-      $("#past").hide();
-      $("#present").hide();
-      $("#friends").hide();
-      $("#beyond").hide();
-    });
+ $('#sitemap-open').on('click', function() {
+  $("#sitemap").slideDown("slow", function() {
+   console.log("sitemap opened");
+   $("#past").hide();
+   $("#present").hide();
+   $("#friends").hide();
+   $("#beyond").hide();
   });
+ });
 
-  $('#sitemap-close').on('click', function() {
-    $("#past").show();
-    $("#present").show();
-    $("#friends").show();
-    $("#beyond").show();
-    $("#sitemap").slideUp("slow", function() {
-      console.log("sitemap closed");
-    });
+ $('#sitemap-close').on('click', function() {
+  $("#past").show();
+  $("#present").show();
+  $("#friends").show();
+  $("#beyond").show();
+  $("#sitemap").slideUp("slow", function() {
+   console.log("sitemap closed");
   });
+ });
+
+ window.onscroll = function(e) {
+  var _cur_top = $(window).scrollTop();
+  // called when the window is scrolled.
+  if (!sitemap_hidden && _cur_top >= $('.nav-wrapper').height()){
+   $("#sitemap-text").hide("slow", function() {
+        sitemap_hidden = true;
+   });
+  }
+
+  if(sitemap_hidden && _cur_top < $('.nav-wrapper').height()){
+   $("#sitemap-text").show("slow", function() {
+        sitemap_hidden = false;
+   });
+  }
+ }
 
 })(jQuery); // end of jQuery name space
